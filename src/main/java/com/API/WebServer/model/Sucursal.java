0a96b07a005434b9dbede3274bbc1ea180bbc715
alisson <alissonfranco56@gmail.com>
2026-09-18 16:13:43 -0500

    Revert "feat: creación de repositorio de la sucursal"
    This reverts commit f3dd6f6ac5faf949aadb19422700abb6f1947656.

--- src/main/java/com/API/WebServer/model/Sucursal.java
+++
@@ -1,9 +0,0 @@
-package com.API.WebServer.model;
-
-public record Sucursal(
-        Integer idSucursal,
-        String nombre,
-        String direccion,
-        String telefono
-) {
-}
--- src/main/java/com/API/WebServer/repository/ProveedorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.API.WebServer.repository;
-
-import com.API.WebServer.model.Proveedor;
-import java.util.List;
-
-public interface ProveedorRepository {
-    List<Proveedor> listar();
-}
