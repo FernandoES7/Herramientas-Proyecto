19732923a69e0a3fd4c7b054fba817892445c7c4
José Ortega <jomi221108@gmail.com>
2026-09-19 10:01:34 -0500

    feat(proveedor): crear interfaz del repositorio de proveedor

---
+++ src/main/java/com/API/WebServer/repository/ProveedorRepository.java
@@ -0,0 +1,10 @@
+package com.API.WebServer.repository;
+
+import java.util.List;
+
+import com.API.WebServer.model.Proveedor;
+
+public interface ProveedorRepository {
+
+    List<Proveedor> listar();
+}
